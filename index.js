0367117d2d4dfc5383c3eba56174240428e0d89f
Pamir Perdeci <pamirperdeci@gmail.com>
2023-10-12 13:01:31 -0400

    fixed duplicate index file issue that was not letting server run

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // require your server and launch it here
+// require your server and launch it here
+const server = require('./api/server')
+const express = require('express')
+const port = 8000;
+
+
+server.listen(port, ()=>{
+    console.log('hello')
+
+})
