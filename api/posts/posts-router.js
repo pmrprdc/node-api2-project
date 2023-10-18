bfa587397b87af076bd30933f1056db65123f83f
Pamir Perdeci <pamirperdeci@gmail.com>
2023-10-18 14:26:28 -0400

    moved endpoints to their own router

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,61 @@
 // implement your posts router here
+const express = require('express')
+const Posts = require('./posts-model')
+const Router = express.Router();
+
+
+Router.get('/',  async (req,res)=>{
+    const posts = await Posts.find()
+    res.status(200).json(posts)
+})
+
+
+
+Router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const post = await Posts.findById(id);
+        console.log(post)
+        if (!post) {
+            // Handle the case where the post with the given ID is not found.
+            res.status(404).json({
+                message: "does not exists"
+            })
+        } else {
+            res.status(200).json(post);
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "There was an error"
+        });
+    }
+});
+
+
+
+
+Router.post('/', async (req, res) => {
+
+    
+        const {title, contents} = req.body;
+        if(!title ||!contents){
+            res.status(400).json({
+                message: "Title or contents is missing"
+            })
+        }else{
+            const insertedPostId = await Posts.insert({title, contents})
+            res.status(201).json({...req.body, ...insertedPostId})
+        }
+
+    
+
+    
+   
+    
+});
+
+
+
+module.exports= Router;
+
+
--- api/server.js
+++ api/server.js
@@ -3,51 +3,9 @@
 const express = require('express');
 const server = express();
 const Posts = require('./posts/posts-model')
+const Router = require('./posts/posts-router')
 server.use(express.json())
-
-
-server.get('/api/posts',  async (req,res)=>{
-    const posts = await Posts.find()
-    res.status(200).json(posts)
-})
-
-
-
-server.get('/api/posts/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const post = await Posts.findById(id);
-        console.log(post)
-        if (!post) {
-            // Handle the case where the post with the given ID is not found.
-            res.status(404).json({
-                message: "does not exists"
-            })
-        } else {
-            res.status(200).json(post);
-        }
-    } catch (err) {
-        res.status(500).json({
-            message: "There was an error"
-        });
-    }
-});
-
-
-
-
-server.post('/api/posts', async (req, res) => {
-    try {
-        const addedPost = await Posts.insert(req.body)
-        res.status(201).json({...req.body, ...addedPost})
-    } catch (err) {
-        res.status(500).json({
-            message: "There was an error"
-        });
-    }
-});
-
-
+server.use('./api/posts',Router )
 
 
 
